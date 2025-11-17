820e8de4360c3900e60a96fa8e20c28d11ce328e
Edyta Pietrzak <edpiet19@gmail.com>
2025-11-17 12:52:05 +0100

    Poprawki w funkcji sprawdzającej parzystość liczby

--- zad4.py
+++ zad4.py
@@ -2,5 +2,5 @@ def suma_wieksza_lub_rowna(a: int, b: int, c: int) -> bool:
     return (a + b) >= c
 
 
-wynik = suma_wieksza_lub_rowna(3, 4, 7)  # True, bo 3+4=7 >= 7
+wynik = suma_wieksza_lub_rowna(3, 4, 7)
 print(wynik)
