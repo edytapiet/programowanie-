4232242506c6f72d5eb86cd4c1e35918f30ae675
Edyta Pietrzak <edpiet19@gmail.com>
2025-12-13 15:21:03 +0100

    Uporządkowanie gałęzi API

--- zad4.py
+++
@@ -1,6 +0,0 @@
-def suma_wieksza_lub_rowna(a: int, b: int, c: int) -> bool:
-    return (a + b) >= c
-
-
-wynik = suma_wieksza_lub_rowna(3, 4, 7)
-print(wynik)
--- zadc.py
+++
@@ -1,7 +0,0 @@
-def wyswietl_parzyste(lista):
-    for liczba in lista:
-        if liczba % 2 == 0:
-            print(liczba)
-
-liczby = list(range(10))
-wyswietl_parzyste(liczby)
