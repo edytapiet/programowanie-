541a86972a87f14bd76b86fa71eabf545907a8c5
Edyta Pietrzak <edpiet19@gmail.com>
2025-12-15 12:59:41 +0100

    Czyszczenie gałęzi z zadań domowych

--- zad3.py
+++
@@ -1,9 +0,0 @@
-def czy_parzysta(liczba: int) -> bool:
-    return liczba % 2 == 0
-
-wynik = czy_parzysta(8)
-
-if wynik:
-    print("Liczba parzysta")
-else:
-    print("Liczba nieparzysta")
--- zad5.py
+++
@@ -1,8 +0,0 @@
-def sprawdz(lista, liczba):
-    for element in lista:
-        if element == liczba:
-            return True
-    return False
-
-print(sprawdz([10, 20, 30], 20))
-print(sprawdz([10, 20, 30], 5))
