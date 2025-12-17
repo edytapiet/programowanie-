c3126bfd2056f4f8fabbe8139fbed4eb3b1c3ee0
Edyta Pietrzak <edpiet19@gmail.com>
2025-12-17 16:26:03 +0100

    Usunięcie folderu project (przeniesiony na branch modularization)

--- project/order.py
+++
@@ -1,9 +0,0 @@
-import magazine.utils
-
-
-class Order:
-    def __init__(self, number):
-        self.number = number
-
-    def info(self):
-        return f"Zamówienie #{self.number}, {magazine.utils.helper()}"
--- project/utils.py
+++
@@ -1,2 +0,0 @@
-def helper():
-    return "Pomoc z utils"
